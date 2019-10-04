1f995283dc9392b1e43eb596db2fc5ec612a9d7d
Micah Zaayer <micahzaayer@gmail.com>
2019-10-04 11:16:13 -0700

    add ui to collect sentence and word.

--- WordCounter/Models/Program.cs
+++ WordCounter/Models/Program.cs
@@ -7,7 +7,16 @@ namespace WordCounter.Models
     {
         static void Main()
         {
+            Console.WriteLine("Hello! Let's play a game. I'm going to have you input a sentence and then seperately input a word. I will tell you how many times that word is in the sentence");
+            Console.WriteLine("Input a sentence:");
+            string sentence = Console.ReadLine();
+            Console.WriteLine("Input a word:");
+            string word = Console.ReadLine();
+            Counter count = new Counter();
+            count.WordCounter(word, sentence);
+            Console.WriteLine(count.WordCount);
             
+
         }
     }
 }
